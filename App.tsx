4b46ecf0834f6b9eb60cced50e449ff744c0dc8d
Takuya Hosooka <addrblk-reg@yahoo.co.jp>
2025-09-25 00:00:30 +0900

    chore: update dependencies in package.json
    - upgraded expo from ~54.0.9 to ~54.0.10
    - upgraded expo-camera from ^13.9.0 to ^17.0.8
    - upgraded expo-gl from ^13.6.0 to ^16.0.7
    - updated react and react-dom to ^19.1.1
    - upgraded react-native-web from ^0.21.0 to ^0.21.1

--- App.tsx
+++ App.tsx
@@ -1,26 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import TabNavigator from './src/navigation/TabNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
       <NavigationContainer>
         <TabNavigator/>
       </NavigationContainer>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
