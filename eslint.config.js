6f7f5a3731ea10aac16cb61bc6e3b803435e4689
Takuya Hosooka <addrblk-reg@yahoo.co.jp>
2026-07-05 12:56:41 +0900

    fix: add 'no-void' rule to ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,7 @@ module.exports = [
       '@typescript-eslint/no-shadow': 'error',
       'no-shadow': 'off',
       'no-undef': 'off',
+      'no-void': ['error', { allowAsStatement: true }],
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
